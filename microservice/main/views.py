e63676b8da2866a432f1a5358c8c99e89a336796
sasha novikov <novikov.ae@gmail.com>
2023-03-05 16:10:05 +0300

    url and view scheme

--- microservice/main/views.py
+++ microservice/main/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class LoginView(View):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class RegistrationView(View):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ConfirmationView(View):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
--- microservice/microservice/urls.py
+++ microservice/microservice/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
+from main.views import LoginView, RegistrationView, ConfirmationView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', LoginView.as_view(), name='login'),
+    # path('logout', name='logout'),
+    path('registartion', RegistrationView.as_view(), name='registration'),
+    path('registartion/<int:id>', ConfirmationView.as_view(), name='confirmation'),
 ]
