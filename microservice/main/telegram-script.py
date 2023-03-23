e24c696c61178fe1c8092466542c0e77a6c23d15
sasha novikov <novikov.ae@gmail.com>
2023-03-23 18:37:18 +0300

    telegram-script threading

--- microservice/main/telegram-script.py
+++ microservice/main/telegram-script.py
@@ -1,6 +1,7 @@
 from telegram.client import Telegram
 from main.selfkeys import api_id, api_hash, phone, SECRET_KEY
 import time
+from threading import Thread
 
 
 def main():
@@ -15,17 +16,25 @@ def main():
     response = tg.get_chats()
     response.wait()
     chats = response.update['chat_ids']
+    chat_history = []
     for chat_id in chats:
-        message_id = 0
-        while True:
-            chat_messages = tg.get_chat_history(chat_id, limit=100, from_message_id=message_id)
-            chat_messages.wait()
-            if chat_messages.error or not chat_messages.update['messages']:
-                break
-            message_id = chat_messages.update['messages'][0]['id']
-            print(chat_id)
-            time.sleep(0.1)
+        thread = Thread(target=parse, args=(chat_id, tg))
+        chat_history.append(thread.run())
+    for thread in chat_history:
+        print(thread)
     tg.stop()
+
+
+def parse(chat_id, tg):
+    message_id = 0
+    while True:
+        chat_messages = tg.get_chat_history(chat_id, limit=100, from_message_id=message_id)
+        chat_messages.wait()
+        if chat_messages.error or not chat_messages.update['messages']:
+            break
+        message_id = chat_messages.update['messages'][0]['id']
+        print(chat_id)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
